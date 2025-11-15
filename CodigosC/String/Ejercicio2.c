4ee8c9e9671ebc6bd8243ec4673af2b2d0fde6fb
Brambilla juan <brambilla9533@gmail.com>
2025-11-15 15:18:02 -0300

    Create Ejercicio2.c

---
+++ CodigosC/String/Ejercicio2.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <ctype.h> 
+
+void pasaMinuscula(char texto[]);  
+
+int main() {
+    char cadena[100];
+
+    printf("Ingrese un texto: ");
+    fgets(cadena, 100, stdin);
+
+    int i = 0;
+    while (cadena[i] != '\0') {
+        if (cadena[i] == '\n') {
+            cadena[i] = '\0';
+            break;
+        }
+        i++;
+    }
+
+    pasaMinuscula(cadena);  
+
+    printf("Texto en minusculas: %s\n", cadena);
+
+    return 0;
+}
+
+void pasaMinuscula(char texto[]) {
+    int i = 0;
+    while (texto[i] != '\0') {
+        texto[i] = tolower(texto[i]);
+        i++;
+    }
+}
